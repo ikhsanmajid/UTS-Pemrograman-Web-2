e2ab304570af9691be2be7978b2e749cae3e892c
Ikhsan Majid <ikhsanmajid@gmail.com>
2022-11-06 13:18:57 +0700

    v1.0

--- proses_delete.php
+++
@@ -1,9 +0,0 @@
-<!--
-buat delete alias hapus ya brooo.......!
--->
-<?php
-	include "koneksi.php";
-	$modal_id=$_GET['modal_id'];
-	$modal=mysqli_query($koneksi,"Delete FROM modal WHERE modal_id='$modal_id'");
-	header('location:index.php');
-?>
